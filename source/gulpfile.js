ddeeee4f7a1fd86d72b56b41ba8120c1574c10f8
Jacob Berthelsen <jacob.berthelsen@vml.com>
2019-03-15 12:41:38 -0500

    cleanup gulp file

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -1,111 +1,228 @@
 /* eslint-disable */
 /*jshint esversion: 6*/
 
-var gulp = require('gulp-help')(require('gulp'), { hideDepsMessage: true, afterPrintCallback: cliNotes });
-var bs = require('browser-sync').create();
-var gulpUtil = require('gulp-util');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var sourcemaps = require('gulp-sourcemaps');
-var del = require('del');
-var imagemin = require('gulp-imagemin');
-var sass = require('gulp-sass');
-var sassLint = require('gulp-sass-lint');
-var eslint = require('gulp-eslint');
+// ========= Packages ========= //
 var changed = require('gulp-changed');
-var runSequence = require('run-sequence');
-var merge = require('merge-stream');
-var include = require('gulp-include');
-var Stream = require('stream');
-var newer = require('gulp-newer');
-var browserSync = require('browser-sync').create();
+var eslint = require('gulp-eslint');
+var gulp = require('gulp-help')(require('gulp'), { hideDepsMessage: true, afterPrintCallback: cliNotes });
+var gulpUtil = require('gulp-util');
 var iconfont = require('gulp-iconfont');
 var iconfontCss = require('gulp-iconfont-css');
+var imagemin = require('gulp-imagemin');
+var include = require('gulp-include');
+var merge = require('merge-stream');
+var newer = require('gulp-newer');
+var runSequence = require('run-sequence');
+var sass = require('gulp-sass');
+var sassLint = require('gulp-sass-lint');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
 
+// ========= Global vars ========= //
 var runTimestamp = Math.round(Date.now()/1000);
 var iconFontName = 'themeIcons';
-
-gulpUtil.env.type = 'production';
-gulpUtil.env.build_type = 'build';
-
 var CONFIGS = require('require-dir')('./gulp_config');
 
+// ========= Tasks ========= //
+
+// === Tasks: Default === //
+gulpUtil.env.type = 'production';
+gulpUtil.env.build_type = 'build';
 gulp.task('default', false, ['help']);
 
+// === Tasks: Build === //
 gulp.task('build', 'Production ready build for client code.', (done) => {
-    build(done);
+  build(done);
 });
 
+// === Tasks: Build--dev === //
 gulp.task('build--dev', 'Development ready build for client code.', (done) => {
-    gulpUtil.env.type = 'development';
-    build(done);
+  gulpUtil.env.type = 'development';
+build(done);
 });
 
-gulp.task('browser-sync', ['build--dev', 'watch'], () => {
-  // CSS Files to inject.
-  var files = [
-    '../docroot/modules/features/**/css/*.css',
-    '../docroot/modules/custom/**/css/*.css',
-    '../docroot/themes/custom/**/css/*.css'
-  ];
-
-  /* Uncomment the following lines and modify the domain to use browsersync. */
-  // browserSync.init(files, {
-  //   proxy: 'https://www.local-domain-here.docksal',
-  //   notify: true,
-  //   open: false
-  // });
-});
-
+// === Tasks: Watch === //
 gulp.task('watch', 'Watch source files for changes and build on update', ['build--dev'], () => {
   gulpUtil.env.type = 'development';
-  gulpUtil.env.build_type = 'watch';
+gulpUtil.env.build_type = 'watch';
 
-  var javascriptSources = [];
-  var sassSources = [];
-  var imageSources = [];
-  var fontSources = [];
+var javascriptSources = [];
+var javascriptDest = [];
+var sassSources = [];
+var sassDest = [];
+var imageSources = [];
 
-  Object.keys(CONFIGS).forEach(function (key) {
-    var config = CONFIGS[key];
+Object.keys(CONFIGS).forEach(function (key) {
+  var config = CONFIGS[key];
 
-    if (config.javascript && config.javascript.src && config.javascript.dest) {
-      javascriptSources.push(config.javascript.src);
-    }
+  if (config.javascript && config.javascript.src && config.javascript.dest) {
+    javascriptSources.push(config.javascript.src);
+  }
 
-    if (config.sass && config.sass.src && config.sass.dest) {
-      sassSources.push(config.sass.src);
-    }
+  if (config.javascript && config.javascript.src && config.javascript.dest) {
+    javascriptDest.push(config.javascript.dest);
+  }
 
-    if (config.images && config.images.src && config.images.dest) {
-      imageSources.push(config.images.src);
-    }
+  if (config.sass && config.sass.src && config.sass.dest) {
+    sassSources.push(config.sass.src);
+  }
 
-    if (config.fonts && config.fonts.src && config.fonts.dest) {
-      fontSources.push(config.fonts.src);
-    }
-  });
+  if (config.sass && config.sass.src && config.sass.dest) {
+    sassDest.push(config.sass.dest);
+  }
 
-  gulp.watch(javascriptSources, () => runSequence('_build.javascript', '_js-lint'));
-  gulp.watch(sassSources, () => runSequence('_build.sass'));
-  gulp.watch(imageSources, () => runSequence('_build.images'));
-  gulp.watch(fontSources, () => runSequence('_build.fonts'));
+  if (config.images && config.images.src && config.images.dest) {
+    imageSources.push(config.images.src);
+  }
 });
 
+gulp.watch(javascriptSources, () => runSequence('_build.javascript', '_js-lint'));
+gulp.watch(sassSources, () => runSequence('_build.sass', '_sass-lint'));
+gulp.watch(imageSources, () => runSequence('_build.images'));
+});
+
+// ========= Sub-tasks ========= //
+
+// === Sub-tasks: javascript === //
+gulp.task('_build.javascript', 'Build JavaScript and move to distribute', () => {
+  var tasks = [];
+
+Object.keys(CONFIGS).forEach(function (key) {
+  var config = CONFIGS[key];
+
+  if (config.javascript && config.javascript.src && config.javascript.dest) {
+    tasks.push(gulp.src(config.javascript.src)
+        .pipe(include()).on('error', console.log)
+        .pipe(isBuild() ? gulpUtil.noop() : changed(config.javascript.dest))
+        .pipe(isProd() ? gulpUtil.noop() : sourcemaps.init())
+        .pipe(isProd() ? uglify() : gulpUtil.noop())
+        .pipe(isProd() ? gulpUtil.noop() : sourcemaps.write('./maps'))
+        .pipe(gulp.dest(config.javascript.dest)));
+  }
+});
+
+return merge(tasks);
+});
+
+// === Sub-tasks: js-lint === //
+gulp.task('_js-lint', 'Lint JS to check for style errors', () => {
+  var tasks = [];
+
+Object.keys(CONFIGS).forEach(function (key) {
+  var config = CONFIGS[key];
+
+  if (config.javascript && config.javascript.src && config.javascript.dest) {
+    tasks.push(gulp.src(config.javascript.src)
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError())
+        .on('error', swallowError));
+  }
+});
+
+return merge(tasks);
+});
+
+// === Sub-tasks: sass === //
+gulp.task('_build.sass', 'Build Sass and compile out CSS', () => {
+  var tasks = [];
+
+Object.keys(CONFIGS).forEach(function (key) {
+  var config = CONFIGS[key];
+
+  if (config.sass && config.sass.src && config.sass.dest) {
+    tasks.push(gulp.src(config.sass.src)
+        .pipe(isProd() ? gulpUtil.noop() :  sourcemaps.init())
+        .pipe(isProd() ? sass({
+          outputStyle: 'compressed',
+          includePaths: []
+        }) : sass({
+          noCache: true,
+          outputStyle: "compressed",
+          lineNumbers: false,
+          includePaths: [],
+          sourceMap: true
+        }).on('error', swallowError))
+        .pipe(isProd() ? gulpUtil.noop() : sourcemaps.write('./maps'))
+        .on('error', swallowError)
+        .pipe(gulp.dest(config.sass.dest)));
+  }
+});
+
+return merge(tasks);
+});
+
+// === Sub-tasks: sass lint === //
+gulp.task('_sass-lint', 'Lint Sass to check for style errors', () => {
+  var tasks = [];
+
+Object.keys(CONFIGS).forEach(function (key) {
+  var config = CONFIGS[key];
+
+  if (config.sass && config.sass.src && config.sass.dest) {
+    tasks.push(gulp.src(config.sass.lintSrc)
+        .pipe(sassLint({
+          sasslintConfig: '.sass-lint.yml'
+        }))
+        .pipe(sassLint.format())
+        .pipe(sassLint.failOnError())
+        .on('error', swallowError));
+  }
+});
+
+return merge(tasks);
+});
+
+// === Sub-tasks: images === //
+gulp.task('_build.images', 'Compress and distribute images', () => {
+  var tasks = [];
+
+Object.keys(CONFIGS).forEach(function (key) {
+  var config = CONFIGS[key];
+  if (config.images && config.images.src && config.images.dest) {
+    tasks.push(gulp.src(config.images.src)
+        .pipe(isBuild() ? gulpUtil.noop() : changed(config.images.dest))
+        .pipe(newer(config.images.dest))
+        .pipe(imagemin({progressive: true}))
+        .pipe(gulp.dest(config.images.dest)));
+  }
+});
+
+return merge(tasks);
+});
+
+// === Sub-tasks: fonts === //
+gulp.task('_build.fonts', 'Distribute local fonts', () => {
+  var tasks = [];
+
+Object.keys(CONFIGS).forEach(function (key) {
+  var config = CONFIGS[key];
+  if (config.fonts && config.fonts.src && config.fonts.dest) {
+    tasks.push(gulp.src(config.fonts.src)
+        .pipe(isBuild() ? gulpUtil.noop() : changed(config.fonts.dest))
+        .pipe(newer(config.fonts.dest))
+        .pipe(gulp.dest(config.fonts.dest)));
+  }
+});
+
+return merge(tasks);
+});
+
+// === Sub-tasks: iconFont === //
 gulp.task('_build.iconFont', 'Build the icon font and move to distribute', () => {
   var tasks = [];
-  Object.keys(CONFIGS).forEach(function (key) {
-    var config = CONFIGS[key];
 
-    if (config.iconFont && config.iconFont.src && config.iconFont.dest && config.iconFont.scssFile && config.iconFont.filePath) {
-      tasks.push(gulp.src(config.iconFont.src)
-        // .pipe(include()).on('error', console.log)
+Object.keys(CONFIGS).forEach(function (key) {
+  var config = CONFIGS[key];
+
+  if (config.iconFont && config.iconFont.src && config.iconFont.dest && config.iconFont.scssTemplate && config.iconFont.scssFile && config.iconFont.filePath) {
+    tasks.push(gulp.src(config.iconFont.src)
+        .pipe(include()).on('error', console.log)
         .pipe(iconfontCss({
           fontName: iconFontName,
-          path: 'scss',
+          path: config.iconFont.scssTemplate,
           targetPath: config.iconFont.scssFile,
-          fontPath: config.iconFont.filePath,
+          fontPath: config.iconFont.filePath
         }))
         .pipe(iconfont({
           fontName: iconFontName, // required
@@ -117,146 +234,42 @@ gulp.task('_build.iconFont', 'Build the icon font and move to distribute', () =>
         }))
         .on('glyphs', function (glyphs, options) {
           // CSS templating, e.g.
+          // uncomment to see full logging output
           // console.log(glyphs, options);
         })
         .pipe(gulp.dest(config.iconFont.dest)));
-    }
-  });
-  return merge(tasks);
+  }
 });
 
-
-gulp.task('_build.javascript', 'Build JavaScript and move to distribute', () => {
-  var tasks = [];
-  Object.keys(CONFIGS).forEach(function (key) {
-    var config = CONFIGS[key];
-
-    if (config.javascript && config.javascript.src && config.javascript.dest) {
-      tasks.push(gulp.src(config.javascript.src)
-        .pipe(include()).on('error', console.log)
-        .pipe(isBuild() ? gulpUtil.noop() : changed(config.javascript.dest))
-        .pipe(isProd() ? gulpUtil.noop() : sourcemaps.init())
-        .pipe(isProd() ? uglify() : gulpUtil.noop())
-        .pipe(isProd() ? gulpUtil.noop() : sourcemaps.write('./maps'))
-        .pipe(gulp.dest(config.javascript.dest)));
-    }
-  });
-  return merge(tasks);
+return merge(tasks);
 });
 
-gulp.task('_sass-lint', 'Lint Sass to check for style errors', () => {
-  var tasks = [];
-  Object.keys(CONFIGS).forEach(function (key) {
-    var config = CONFIGS[key];
+// ========= Functions ========= //
 
-    if (config.sass && config.sass.src && config.sass.dest) {
-      tasks.push(gulp.src(config.sass.lintSrc)
-        .pipe(sassLint({
-          sasslintConfig: '.sass-lint.yml'
-        }))
-        .pipe(sassLint.format())
-        .pipe(sassLint.failOnError())
-        .on('error', swallowError));
-    }
-  });
-  return merge(tasks);
-});
-
-gulp.task('_js-lint', 'Lint JS to check for style errors', () => {
-  var tasks = [];
-  Object.keys(CONFIGS).forEach(function (key) {
-    var config = CONFIGS[key];
-
-    if (config.javascript && config.javascript.src && config.javascript.dest) {
-      tasks.push(gulp.src(config.javascript.src)
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError())
-        .on('error', swallowError));
-    }
-  });
-  return merge(tasks);
-});
-
-gulp.task('_build.sass', 'Build Sass and compile out CSS', () => {
-  var tasks = [];
-  Object.keys(CONFIGS).forEach(function (key) {
-    var config = CONFIGS[key];
-
-    if (config.sass && config.sass.src && config.sass.dest) {
-      tasks.push(gulp.src(config.sass.src)
-            .pipe(isProd() ? gulpUtil.noop() :  sourcemaps.init())
-            .pipe(isProd() ? sass({
-              outputStyle: 'compressed',
-              includePaths: []
-            }) : sass({
-              noCache: true,
-              outputStyle: "compressed",
-              lineNumbers: false,
-              includePaths: [],
-              sourceMap: true
-            }).on('error', swallowError))
-            .pipe(isProd() ? gulpUtil.noop() : sourcemaps.write('./maps'))
-            .on('error', swallowError)
-            .pipe(gulp.dest(config.sass.dest)));
-    }
-  });
-  return merge(tasks);
-});
-
-gulp.task('_build.images', 'Compress and distribute images', () => {
-
-  var tasks = [];
-
-  Object.keys(CONFIGS).forEach(function (key) {
-    var config = CONFIGS[key];
-    if (config.images && config.images.src && config.images.dest) {
-      tasks.push(gulp.src(config.images.src)
-        .pipe(isBuild() ? gulpUtil.noop() : changed(config.images.dest))
-        .pipe(newer(config.images.dest))
-        .pipe(imagemin({progressive: true}))
-        .pipe(gulp.dest(config.images.dest)));
-    }
-  });
-  return merge(tasks);
-});
-
-gulp.task('_build.fonts', 'Distribute local fonts', () => {
-
-  var tasks = [];
-
-  Object.keys(CONFIGS).forEach(function (key) {
-    var config = CONFIGS[key];
-    if (config.fonts && config.fonts.src && config.fonts.dest) {
-      tasks.push(gulp.src(config.fonts.src)
-          .pipe(isBuild() ? gulpUtil.noop() : changed(config.fonts.dest))
-          .pipe(newer(config.fonts.dest))
-          .pipe(gulp.dest(config.fonts.dest)));
-    }
-  });
-  return merge(tasks);
-});
-
+// === Functions: build === //
 function build(done) {
   runSequence(
-      ['_build.fonts', '_build.iconFont', '_build.images', '_build.javascript', '_js-lint', '_build.sass', '_sass-lint'],
-      done
+      ['_build.fonts', '_build.iconFont', '_build.images', '_build.javascript', '_js-lint', '_build.sass', '_sass-lint'], done
   );
 }
 
+// === Functions: swallowError === //
 function swallowError(error) {
-    console.log(error.toString());
-
-    this.emit('end');
+  console.log(error.toString());
+  this.emit('end');
 }
 
+// === Functions: isProd === //
 function isProd() {
-    return gulpUtil.env.type === 'production';
-}
-function isBuild() {
-    return gulpUtil.env.build_type === 'build';
+  return gulpUtil.env.type === 'production';
 }
 
+// === Functions: isBuild === //
+function isBuild() {
+  return gulpUtil.env.build_type === 'build';
+}
+
+// === Functions: cliNotes === //
 function cliNotes() {
-    console.log('  * _tasks are private sub tasks. Only use if necessary. *\n\n\n');
+  console.log('  * _tasks are private sub tasks. Only use if necessary. *');
 }
