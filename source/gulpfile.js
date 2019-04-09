dd333ad273b555903320c1fd1c5382f3ab219f8d
Jacob Berthelsen <jacob.berthelsen@vml.com>
2019-04-09 10:55:50 -0500

    more notes

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -1,16 +1,34 @@
 /*
+ * V 2.1
+ *
  * This file is set up to run standard build and testing tasks over
  * ALL themes in the theme/custom directory.
  *
+ * 😃 No changes are needed on startup. 😃
+ *
+ * !!! Files are NOT minified !!!
+ * Minification should be handled through Advanced Aggregation Drupal module
+ *
+ * === Piping ===
  * Provided in this file are the default path and task configurations.
+ * Files follows the same path as where it started from.
+ * scss to css - js to js - images to images - icons to icons - fonts to fonts
  *
- * *** 😃 No changes are needed on startup. 😃 ***
+ * `source/themes/custom/project/js`
+ *  is piped to
+ * `docroot/themes/custom/project/js`
  *
- * Preset tasks:
+ * === Example files ===
+ * An `example.` prefix can be added to scss and js files.
+ * The prefixed files will not be linted or piped.
+ * ex: `styles/libraries/node/example.full.scss`
+ *
+ * === Preset tasks ===
  * gulp = runs all build tasks
  * gulp test = runs all testing tasks
  * gulp watch = starts the watchers (builds & tests)
  *
+ * === Gulp file structure ===
  * 1. Configs
  * 2. Build Tasks
  * 3. Testing Tasks
@@ -34,15 +52,23 @@ const { src, dest, watch, parallel, series } = require('gulp');
  * 1. Configs                         *
  **************************************/
 
+// === Theme name === //
+// Gets an array of all the theme names
 let files = fs.readdirSync('themes/custom/');
 let config = [];
 
+// Removes hidden files and update the key value
 for (const value of files) {
   if (value.startsWith(".")) {
     continue;
   }
   config.push({themeName: value});
 }
+
+// === Custom === //
+// overrides the dest path after the theme name
+// ../docroot/themes/custom/themeName/customDest/*
+const customDest = '';
 
 /**************************************
  * 2. Build Tasks                     *
@@ -60,7 +86,7 @@ function buildSass() {
           sourceMap: true
         }))
         .pipe(sourcemaps.write('./maps'))
-        .pipe(dest('../docroot/themes/custom/' + entry.themeName + '/css'));
+        .pipe(dest('../docroot/themes/custom/' + entry.themeName + '/' + customDest + '/css'));
   });
 
   return merge(tasks);
@@ -72,7 +98,7 @@ function buildJavascript() {
       .pipe(sourcemaps.init())
       .pipe(changed('../docroot/themes/custom/' + entry.themeName + '/js'))
       .pipe(sourcemaps.write('./maps'))
-      .pipe(dest('../docroot/themes/custom/' + entry.themeName + '/js'));
+      .pipe(dest('../docroot/themes/custom/' + '/' + customDest + '/js'));
   });
 
   return merge(tasks);
@@ -83,7 +109,7 @@ function buildImages() {
     return src('themes/custom/' + entry.themeName + '/images/**/*')
       .pipe(changed(entry.destDir + '/img'))
       .pipe(imagemin({progressive: true}))
-      .pipe(dest('../docroot/themes/custom/' + entry.themeName + '/images'));
+      .pipe(dest('../docroot/themes/custom/' + '/' + customDest + '/images'));
   });
 
   return merge(tasks);
@@ -93,7 +119,7 @@ function buildFonts() {
   const tasks = config.map(function(entry) {
     return src('themes/custom/' + entry.themeName + '/fonts/**/*')
       .pipe(changed('../docroot/themes/custom/' + entry.themeName + '/fonts'))
-      .pipe(dest('../docroot/themes/custom/' + entry.themeName + '/fonts'));
+      .pipe(dest('../docroot/themes/custom/' + '/' + customDest + '/fonts'));
   });
 
   return merge(tasks);
@@ -108,7 +134,7 @@ function buildIcons() {
         fontName: 'themeIcons',
         path: 'themes/custom/' + entry.themeName + '/styles/vendor/_icons-template.scss',
         targetPath: '../../../../../../source/themes/custom/' + entry.themeName + '/styles/vendor/_icons.scss',
-        fontPath: 'themes/custom/' + entry.themeName + '/fonts/icons'
+        fontPath: 'themes/custom/' + entry.themeName + '/' + customDest + '/fonts/icons'
       }))
       .pipe(iconfont({
         fontName: 'themeIcons',
@@ -118,7 +144,7 @@ function buildIcons() {
         normalize: true,
         fontHeight: 1001
       }))
-      .pipe(dest('../docroot/themes/custom/' + entry.themeName + '/fonts/icons'));
+      .pipe(dest('../docroot/themes/custom/' + entry.themeName + '/' + customDest + '/fonts/icons'));
   });
 
   return merge(tasks);
