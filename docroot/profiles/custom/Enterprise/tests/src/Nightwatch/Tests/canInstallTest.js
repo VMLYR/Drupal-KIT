7b44d7be4ceb5ff9dd51d5efb4494b168c7fa310
Michael Smith <michael.smith@vmlyr.com>
2023-03-28 10:00:49 -0500

    Add a nightwatch test for enteprise install profile.

---
+++ docroot/profiles/custom/Enterprise/tests/src/Nightwatch/Tests/canInstallTest.js
@@ -0,0 +1,30 @@
+module.exports = {
+  '@tags': ['enterprise'],
+  before: function(browser) {
+  },
+  after: function(browser) {
+  },
+  'Run the install profile': (browser) => {
+    browser
+      .drupalRelativeURL('core/install.php')
+      .waitForElementVisible('body', 1000)
+      .assert.visible('.install-page')      
+      .waitForElementVisible('.page-wrapper', 1000)
+      .assert.visible('.site-name')
+      .assert.containsText('.site-name', 'Enterprise')
+      .click('#edit-submit')
+      .waitForElementVisible('body', 1000)
+      .assert.containsText('.heading-c', 'Installing Enterprise')
+      .waitForElementVisible('.install-configure-form', 300000)
+      .setValue('input#edit-site-name', "Test Site")
+      .setValue('input#edit-site-mail', "noreply@vmlyr.com")
+      .setValue('input#edit-account-name', "admin")
+      .setValue('input#edit-account-pass-pass1', "CorrectHorseBatteryStaple")
+      .setValue('input#edit-account-pass-pass1', "CorrectHorseBatteryStaple")
+      .setValue('input#edit-account-mail', "noreply@vmlyr.com")
+      .click('#edit-submit')
+      .waitForElementVisible('.layout-container', 50000)
+      .end();
+  },
+
+};
