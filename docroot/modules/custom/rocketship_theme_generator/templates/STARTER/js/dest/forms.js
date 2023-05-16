760e58ebfdc54a892680a5b0122c487f01aa64f4
Mike Kuykendall <mike.kuykendall@vmlyr.com>
2023-05-16 15:19:18 -0500

    Add new page working.

--- docroot/modules/custom/rocketship_theme_generator/templates/STARTER/js/dest/forms.js
+++ docroot/modules/custom/rocketship_theme_generator/templates/STARTER/js/dest/forms.js
@@ -17,36 +17,36 @@
         var l = a(this);
         l.closest(s).removeClass("is-active").find("label").removeClass("is-active"), "search_block_form" == l.attr("name") && l.parent().removeClass("is-active").find("label").removeClass("is-active"), l.val() ? (l.closest(s).addClass("has-value").find("label").addClass("has-value"), "search_block_form" == l.attr("name") && l.parent().addClass("has-value").find("label").addClass("has-value")) : (l.closest(s).removeClass("has-value").find("label").removeClass("has-value"), "search_block_form" == l.attr("name") && l.parent().removeClass("has-value").find("label").removeClass("has-value"))
       })), e.val() && (e.closest(s).addClass("has-value").find("label").addClass("has-value"), "search_block_form" == e.attr("name") && e.parent().addClass("has-value").find("label").addClass("has-value"))
-    }), s.length && once("js-once-textarea", s).each((function () {
-      var e = a(this), s = ".form__element";
+    }), s.length && once("js-once-textarea", s).forEach((s) => {
+      var e = a(s), s = ".form__element";
       e.closest(s).length < 1 && (s = ".form-wrapper"), e.focus((function (e) {
         a(this).closest(s).addClass("is-active").find("label").addClass("is-active")
       })).blur((function (e) {
         var l = a(this);
         l.closest(s).removeClass("is-active").find("label").removeClass("is-active"), l.val() ? l.closest(s).addClass("has-value").find("label").addClass("has-value") : l.closest(s).removeClass("has-value").find("label").removeClass("has-value")
       })), e.val() && e.closest(s).addClass("has-value").find("label").addClass("has-value")
-    })), l.length && once("js-once-select", l).each((function () {
-      var e = a(this), s = ".form__element";
+    }), l.length && once("js-once-select", l).forEach((l) => {
+      var e = a(l), s = ".form__element";
       e.closest(s).length < 1 && (s = ".form-wrapper"), e.focus((function (e) {
-        a(this).parent().parent().find("label").addClass("is-active")
+        a(l).parent().parent().find("label").addClass("is-active")
       })).blur((function (e) {
-        var l = a(this);
+        var l = a(l);
         l.closest(s).removeClass("is-active").find("label").removeClass("is-active"), l.val() ? l.closest(s).addClass("has-value").find("label").addClass("has-value") : l.closest(s).removeClass("has-value").find("label").removeClass("has-value")
       })), e.val() && (("object" == typeof e.val() && e.val().length) > 0 && e.closest(s).addClass("has-value").find("label").addClass("has-value"), "object" != typeof e.val() && e.closest(s).addClass("has-value").find("label").addClass("has-value"))
-    }))
+    })
   }, t.customSelect = function (e) {
-    once("js-once-select-wrap", e).each((function () {
-      a(this).closest(".form__dropdown").length < 1 && a(this).wrap('<div class="form__dropdown"></div>')
-    }))
+    once("js-once-select-wrap", e).forEach((e) => {
+      a(e).closest(".form__dropdown").length < 1 && a(e).wrap('<div class="form__dropdown"></div>')
+    })
   }, t.textareaScroll = function (e) {
-    once("js-once-textarea-scroll", e).each((function () {
-      var e = a(this), s = function (a) {
+    once("js-once-textarea-scroll", e).forEach((e) => {
+      var e = a(e), s = function (a) {
         a.scrollTop > 0 ? e.closest(".form__element").addClass("js-scrolling") : e.closest(".form__element").removeClass("js-scrolling")
       };
       s(e[0]), e.on("scroll", (function () {
-        s(this)
+        s(e)
       }))
-    }))
+    })
   }, t.stateCheck = function () {
     a(l).on("state:required", (function (e) {
       if (e.trigger && "function" == typeof a(e.target).isWebform && a(e.target).isWebform()) {
