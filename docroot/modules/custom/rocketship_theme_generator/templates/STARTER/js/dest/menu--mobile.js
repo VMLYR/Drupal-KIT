a532a19a44e2b28056f9e02e843bc8f5809b4f3e
Mike Kuykendall <mike.kuykendall@vmlyr.com>
2023-05-16 12:12:13 -0500

    Update rocketship theme generator files to work with D10; once, drupal_get_path, render, twig spaceless.

--- docroot/modules/custom/rocketship_theme_generator/templates/STARTER/js/dest/menu--mobile.js
+++ docroot/modules/custom/rocketship_theme_generator/templates/STARTER/js/dest/menu--mobile.js
@@ -1,1 +1,30 @@
-!function(e,n,o,s){"use strict";void 0===o.rocketshipUI&&(o.rocketshipUI={});var i=o.rocketshipUI;n.behaviors.rocketshipUIMobileMenu={attach:function(n,o){var s=e(".wrapper--navigation",n),a=s.find(".navigation__toggle-expand",n);a.length&&s.length&&i.mobileMenu(a,s)}},i.mobileMenu=function(n,s){n.once("js-once-mobilemenu").click((function(e){"xs"==i.screen&&(s.hasClass("js-open")?s.removeClass("js-open"):s.addClass("js-open")),e.preventDefault()})),e(o).on("resize",(function(){"xs"!=i.screen&&s.hasClass("js-open")&&s.removeClass("js-open")}))}}(jQuery,Drupal,window,document);
+(function (e, n, o, s) {
+  "use strict";
+  void 0 === o.rocketshipUI && (o.rocketshipUI = {});
+  var i = o.rocketshipUI;
+  n.behaviors.rocketshipUIMobileMenu = {
+    attach: function (n, o) {
+      var s = e(".wrapper--navigation", n), a = s.find(".navigation__toggle-expand", n);
+      a.length && s.length && i.mobileMenu(a, s)
+    }
+  }, i.mobileMenu = function (n, s) {
+    var clickHandler = function (e) {
+      "xs" == i.screen && (s.classList.contains("js-open") ? s.classList.remove("js-open") : s.classList.add("js-open")), e.preventDefault()
+    };
+
+    var once = function (element, event, handler) {
+      var wrappedHandler = function () {
+        handler.apply(this, arguments);
+        element.removeEventListener(event, wrappedHandler);
+      };
+
+      element.addEventListener(event, wrappedHandler);
+    };
+
+    once(n[0], 'click', clickHandler);
+
+    e(o).on("resize", function () {
+      "xs" != i.screen && s.classList.contains("js-open") && s.classList.remove("js-open")
+    });
+  }
+})(jQuery, Drupal, window, document);
